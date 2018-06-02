7af4501d95fb28ae324ad1851649b120ad9cd10b
Andrei Mateescu <andrei@amateescu.me>
2018-06-02 13:58:30 +0300

    Add test coverage for directory traversal.

--- tests/IpfsStreamTest.php
+++ tests/IpfsStreamTest.php
@@ -22,4 +22,48 @@ class IpfsStreamTest extends TestCase
 
         $this->assertContains('ipfs', \stream_get_wrappers());
     }
+
+    /**
+     * @covers ::dir_closedir()
+     * @covers ::dir_opendir()
+     * @covers ::dir_readdir()
+     * @covers ::dir_rewinddir()
+     */
+    public function testDirectoryTraversal()
+    {
+        // go-ipfs initializes a directory structure by default with this hash:
+        // QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG and the following
+        // contents:
+        // - about
+        // - contact
+        // - help
+        // - quick-start
+        // - readme
+        // - security-notes
+        $dir_uri = 'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG';
+
+        // Open a known directory, and proceed to read its contents.
+        $files = $dirs = [];
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir_uri));
+        foreach ($iterator as $file) {
+            $type = $file->getType();
+
+            if ($type === 'file') {
+                $files[] = (string) $file;
+            } elseif ($type === 'dir') {
+                $dirs[] = (string) $file;
+            }
+        }
+
+        $expected = [
+            'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/about',
+            'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/contact',
+            'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/help',
+            'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/quick-start',
+            'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme',
+            'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/security-notes',
+        ];
+        $this->assertSame($expected, $files);
+        $this->assertSame([], $dirs);
+    }
 }
